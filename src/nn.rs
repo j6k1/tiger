b7f1ebfadf9f0e3ab282391dc9ee8864be73e269
j6k1 <nanasi18@gmail.com>
2024-02-24 12:40:33 +0900

    Bug Fixes Fixed an error in the sign of a calculation formula.

--- src/nn.rs
+++ src/nn.rs
@@ -20,7 +20,7 @@ use nncombinator::layer::activation::ActivationLayer;
 use nncombinator::layer::batchnormalization::BatchNormalizationLayerBuilder;
 use nncombinator::lossfunction::{CrossEntropy};
 use nncombinator::ope::UnitValue;
-use nncombinator::optimizer::{MomentumSGD, SGD};
+use nncombinator::optimizer::{MomentumSGD};
 use nncombinator::persistence::{BinFilePersistence, Linear, Persistence, PersistenceType, SaveToFile};
 use packedsfen::hcpe::reader::HcpeReader;
 use packedsfen::traits::Reader;
@@ -187,7 +187,7 @@ pub struct Trainer<M>
     where M: BatchNeuralNetwork<f32,DeviceGpu<f32>,BinFilePersistence<f32>,Linear,Arr<f32,2515>,Arr<f32,1>> {
 
     nn:M,
-    optimizer:SGD<f32>,
+    optimizer:MomentumSGD<f32>,
     nn_path:String,
     nnsavedir:String,
     packed_sfen_reader:PackedSfenReader,
@@ -255,7 +255,7 @@ impl TrainerCreator {
 
         Ok(Trainer {
             nn:nn,
-            optimizer:SGD::new(learning_rate),
+            optimizer:MomentumSGD::new(learning_rate),
             nn_path: nn_path,
             nnsavedir: save_dir,
             packed_sfen_reader:PackedSfenReader::new(),
@@ -516,7 +516,7 @@ impl<M> Trainer<M> where M: BatchNeuralNetwork<f32,DeviceGpu<f32>,BinFilePersist
                             rate
                         }
                         GameEndState::Lose => {
-                            0.5 - 0.5 * -rate
+                            0.5 - 0.5 * rate
                         },
                         _ => 0.5f32
                     };
