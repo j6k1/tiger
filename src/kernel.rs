454c53f63eca3e051db80f97ebf48b6980b10770
j6k1 <nanasi18@gmail.com>
2025-10-03 20:33:36 +0900

    Bug Fix The calculation formula was still incorrect, so I fixed it again.

--- src/kernel.rs
+++ src/kernel.rs
@@ -216,7 +216,7 @@ impl<'a,A,const NI:usize,const NO:usize> Kernel for TransformFeaturesForwardBatc
 
     fn launch_config(&self, args: &Self::Args) -> KernelLaunchConfig {
         KernelLaunchConfig {
-            grid_dim: dim3 { x: (NO as c_uint * 2 + 32 - 1) * args.batch_size  as c_uint / 32, y: 1, z: 1 },
+            grid_dim: dim3 { x: (NO * 2 * args.batch_size + 32 - 1) as c_uint / 32, y: 1, z: 1 },
             block_dim: dim3 { x: 32, y: 1, z: 32},
             shared_memory_size: 0
         }
