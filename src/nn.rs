22b798657904ac90acc112468e5449b3bca33965
j6k1 <nanasi18@gmail.com>
2025-11-03 11:15:00 +0900

    Adjust the label value entered when checkmate or resignation occurs

--- src/nn.rs
+++ src/nn.rs
@@ -1402,9 +1402,9 @@ impl<M,A> Trainer<M,A>
                         };
 
                         if score == 30000 {
-                            0.995
+                            0.95
                         } else if score == -30000 {
-                            0.005
+                            0.05
                         } else {
                             let r = t * lambda + Self::sigmoid(score as f32) * (1. - lambda);
 
