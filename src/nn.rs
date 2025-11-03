133ddb86978b358f5dedf74216052507246540c4
j6k1 <nanasi18@gmail.com>
2025-11-03 13:54:18 +0900

    Parameter adjustment

--- src/nn.rs
+++ src/nn.rs
@@ -823,7 +823,7 @@ impl EvalutorCreator {
         let n1 = Normal::<f32>::new(0.0, (2f32 / (ACTIVE_INDICES + 256) as f32).sqrt()).unwrap();
         let n2 = Normal::<f32>::new(0.0, (2f32 / (512f32 + 32f32)).sqrt()).unwrap();
         let n3 = Normal::<f32>::new(0.0, (2f32 / (32f32 + 32f32)).sqrt()).unwrap();
-        let n4 = Normal::<f32>::new(0.0, 0.05).unwrap();
+        let n4 = Normal::<f32>::new(0.0, 0.07).unwrap();
 
         let device = DeviceCpu::new()?;
 
@@ -940,7 +940,7 @@ impl TrainerCreator {
         let n1 = Normal::<f32>::new(0.0, (2f32 / (ACTIVE_INDICES + 256) as f32).sqrt()).unwrap();
         let n2 = Normal::<f32>::new(0.0, (2f32 / (512f32 + 32f32)).sqrt()).unwrap();
         let n3 = Normal::<f32>::new(0.0, (2f32 / (32f32 + 32f32)).sqrt()).unwrap();
-        let n4 = Normal::<f32>::new(0.0, 0.05).unwrap();
+        let n4 = Normal::<f32>::new(0.0, 0.07).unwrap();
 
         let device = DeviceGpu::new(&allocator)?;
 
