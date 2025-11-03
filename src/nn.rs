8809c2a4526ada730919e96184292b0a7986b1e5
j6k1 <nanasi18@gmail.com>
2025-11-04 07:16:58 +0900

    Parameter tuning

--- src/nn.rs
+++ src/nn.rs
@@ -821,9 +821,9 @@ impl EvalutorCreator {
         let mut rnd = XorShiftRng::from_seed(rnd.gen());
 
         let n1 = Normal::<f32>::new(0.0, (2f32 / (ACTIVE_INDICES + 256) as f32).sqrt()).unwrap();
-        let n2 = Normal::<f32>::new(0.0, (2f32 / ((512f32 + 32f32)).sqrt() * 1.2)).unwrap();
+        let n2 = Normal::<f32>::new(0.0, (2f32 / ((512f32 + 32f32) * 1.2)).sqrt()).unwrap();
         let n3 = Normal::<f32>::new(0.0, (2f32 / (32f32 + 32f32)).sqrt()).unwrap();
-        let n4 = Normal::<f32>::new(0.0, 1f32 / ((32f32 + 1f32).sqrt())).unwrap();
+        let n4 = Normal::<f32>::new(0.0, 1f32 / ((32f32 + 1f32).sqrt() * 2.0)).unwrap();
 
         let device = DeviceCpu::new()?;
 
@@ -938,9 +938,9 @@ impl TrainerCreator {
         let mut rnd = XorShiftRng::from_seed(rnd.gen());
 
         let n1 = Normal::<f32>::new(0.0, (2f32 / (ACTIVE_INDICES + 256) as f32).sqrt()).unwrap();
-        let n2 = Normal::<f32>::new(0.0, (2f32 / ((512f32 + 32f32)).sqrt() * 1.2)).unwrap();
+        let n2 = Normal::<f32>::new(0.0, (2f32 / ((512f32 + 32f32) * 1.2)).sqrt()).unwrap();
         let n3 = Normal::<f32>::new(0.0, (2f32 / (32f32 + 32f32)).sqrt()).unwrap();
-        let n4 = Normal::<f32>::new(0.0, 1f32 / ((32f32 + 1f32).sqrt())).unwrap();
+        let n4 = Normal::<f32>::new(0.0, 1f32 / ((32f32 + 1f32).sqrt() * 2.0)).unwrap();
 
         let device = DeviceGpu::new(&allocator)?;
 
